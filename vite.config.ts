834e5609f3a8f1cbabd99a8275031eb643090b7b
praneeththilina <86355665+praneeththilina@users.noreply.github.com>
2026-02-04 21:29:09 +0530

    Update vite.config.ts

--- vite.config.ts
+++ vite.config.ts
@@ -1,11 +1,12 @@
-import path from 'path';
-import { defineConfig, loadEnv } from 'vite';
-import react from '@vitejs/plugin-react';
-import { VitePWA } from 'vite-plugin-pwa';
+import path from 'path'
+import { defineConfig, loadEnv } from 'vite'
+import react from '@vitejs/plugin-react'
+import { VitePWA } from 'vite-plugin-pwa'
 
 export default defineConfig(({ mode }) => {
-    const env = loadEnv(mode, '.', '');
+    const env = loadEnv(mode, '.', '')
     return {
+      base: '/partflow-pro/', 
       server: {
         port: 3000,
         host: '0.0.0.0',
@@ -44,5 +45,5 @@ export default defineConfig(({ mode }) => {
           '@': path.resolve(__dirname, '.'),
         }
       }
-    };
-});
+    }
+})
